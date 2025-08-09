dbbbd431d91c1f6d2570c0643b82d4930b52ddad
yupcoding1 <mdhuzefabbasi@gmail.com>
2025-08-09 15:20:49 +0530

    changed names

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Mohammed Abbasi - Portfolio',
+  description: 'A showcase of my work and skills as a software engineer',
   generator: 'v0.dev',
 }
 
